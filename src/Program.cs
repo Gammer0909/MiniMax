5fc93aea980dad59de5d7d2abd2e633651a18316
Gammer0909 <103458991+Gammer0909@users.noreply.github.com>
2024-03-28 19:56:36 +0000

    Switch to C# for Chess

--- src/Program.cs
+++ src/Program.cs
@@ -7,10 +7,10 @@ namespace Gammer0909.MiniMax;
 
 public class Program {
     public static void Main() {
-
+        
+        // A mess of OOP spaghetti for a C L E A N main method :happy:
         TicTacToeGame game = new TicTacToeGame();
         game.Start();
-
 
     }
 }
--- src/python/chess.py
+++
@@ -1,5 +0,0 @@
-from stockfish import Stockfish
-import os
-
-s = Stockfish("../../../usr/games/stockfish/")
-
